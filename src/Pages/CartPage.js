f698bc3225e1cd7c0803b3f457b221291b2a0e64
behrooz <behrooz.zendedel1992@gmail.com>
2022-04-19 12:48:06 +0430

    update style cartpage

--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -1,18 +1,43 @@
 import Layout from "../Layout/Layout";
 import { useCart } from "../Providers/CartProvider";
+import "./cartPage.css";
 
 const CartPage = () => {
   const { cart } = useCart();
   if (!cart.length)
     return (
-      <main>
-        <h2>cart is empty !</h2>
-      </main>
+      <Layout>
+        <main>
+          <h2>cart is empty !</h2>
+        </main>
+      </Layout>
     );
 
   return (
     <Layout>
-      <main></main>
+      <main className="container">
+        <section className='cartCenter'>
+          <section className="cartItemList">
+            {cart.map((item) => {
+              return (
+                <div className="cartItem">
+                  <div className="itemImage">
+                    <img src={item.image} alt={item.name}></img>
+                  </div>
+                  <div>{item.name}</div>
+                  <div>{item.price * item.quantity}</div>
+                  <div>
+                    <button>remove</button>
+                    <button>{item.quantity}</button>
+                    <button>add</button>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
+          <section className="cartSummery">cart summery</section>
+        </section>
+      </main>
     </Layout>
   );
 };
